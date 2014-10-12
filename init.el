331ae8447a18921923eb13ccdfcc9f18c7d46f99
rho <rho.rhoit@gmail.com>
2014-10-12 11:07:12 +0545

    py-execution mode
    * update ess-style-py-execution
      - renaming th function to py-execution
      - adding auto start python buffer
      - fix the echo command as in interactive mode
      - adding muliline comment handeling

--- init.el
+++ init.el
@@ -394,48 +394,79 @@ See `sort-regexp-fields'."
 ;; (file-name-nondirectory buffer-file-name)))))
 
 ;;----------------------------------------------------------------------
-;; ess style exection for python
+;; ess style execution for python
 (setq selection_flag nil)
-(defun ess-style-py-execution ()
+(defun py-execution (&optional coding)
   "Call python-shell with selected region or current line (if none selected)"
   (interactive)
-  ;; # for the marked section
-  (if (and transient-mark-mode mark-active)
-      (progn ;true
-          (python-shell-send-region (point) (mark))
-          (if selection_flag ;; flag to continue the run mode :D
-              (progn
-                (deactivate-mark)
-                (next-line)
-                (setq selection_flag nil)))
-          (return)))
+
+  ;; start inferior Python process to access *Python* buffer
+  (when (not (get-buffer "*Python*"))
+    ;; (funcall (key-binding (kbd "C-c C-p")))
+    (call-interactively 'run-python)
+    (other-window 1))
+
+  (when (and transient-mark-mode mark-active)
+    (python-shell-send-region (point) (mark))
+    (when selection_flag ;; flag to continue the run mode :D
+      (deactivate-mark)
+      (next-line)
+      (setq selection_flag nil))
+    (return))
 
   ;; auto select the section for execution to avoid error
-  (if (string-match ":[\t\n ]*$" (thing-at-point 'line))
-      (progn
-        (beginning-of-visual-line)
-        (set-mark-command nil)
-        (setq selection_flag t)
-        (if (re-search-forward "\n\n+[^\t ]+?" (point-max) t)
-            (search-backward-regexp "[^\t\n ]\n")
-            (end-of-buffer))
-        (end-of-line)
-        (return)))
+  (when (string-match ":[\t\n ]*$" (thing-at-point 'line))
+    (beginning-of-visual-line)
+    (set-mark-command nil)
+    (setq selection_flag t)
+    (if (re-search-forward "\n\n+[^\t ]+?" (point-max) t)
+        (search-backward-regexp "[^\t\n ]\n")
+      (end-of-buffer))
+    (end-of-line)
+    (return))
 
-  ;; # for un-marked section
-  (if (string-match "^[#\n]" (thing-at-point 'line))
-      ;; check validity of current line
-      (progn
-        (search-forward-regexp "^[^\n\t ]+")
-        (end-of-line)
-        (return)))
+  ;; SECOND PART OF THE SCRIPT
+  (setq current_line
+        ;; NOTE: thing-at-point has \n problem
+        (buffer-substring
+         (line-beginning-position) (line-end-position)))
 
-  (python-shell-send-region (point-at-bol) (point-at-eol))
-  (re-search-forward "[^\n\t ]+")
-  (end-of-visual-line))
+  (when (string-match "^[#\n]" (thing-at-point 'line))
+    ;; check validity of current line
+    (search-forward-regexp "^[^\n\t ]+")
+    (end-of-line)
+    (return))
+
+  ;; for multi line comment # FIXME: jump between the double and single quoted multiline comment :/
+  (when (setq comment_char (string-match "['\"]\\{3\\}" (thing-at-point 'line)))
+    (re-search-forward "['\"]\\{3\\}\n" (point-max) (re-search-forward "\n"))
+    ;;(previous-line) # TEMP solution; avoid the landing in closing comment
+    (end-of-line)
+    (return))
+
+  (when (string= "echo_off" coding)
+    (python-shell-send-region (point-at-bol) (point-at-eol))
+    (return))
+
+  ;; (funcall (key-binding (kbd "RET")))
+  ;; TODO: behave according to the position like coding mode, inspection mode, debug mode
+  (next-line)
+  (beginning-of-visual-line)
+
+  (with-current-buffer "*Python*"
+    (goto-char (point-max))
+    (insert current_line)
+    (funcall (key-binding (kbd "RET")))))
 
 (add-hook 'python-mode-hook
-          (lambda () (local-set-key (kbd "<C-return>") 'ess-style-py-execution)))
+          (lambda () (local-set-key (kbd "<C-return>") 'py-execution)))
+
+(defun py-execution-echo-off()
+  (interactive)
+  (funcall py-execution "echo-off"))
+
+(add-hook 'python-mode-hook
+          (lambda () (local-set-key (kbd "<M-return>") 'py-execution-echo-off)))
 
 ;;----------------------------------------------------------------------
 ;; etags-select
