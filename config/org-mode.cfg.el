bafc50ef25fdc167a5c0d43928eda162b314ceae
rho <rho.rhoit@gmail.com>
2015-02-19 14:26:50 +0545

    adding yasnippet for org-mode
    - org-mode README section
    - org-mode config
      - mouse support
      - refactoring

--- config/org-mode.cfg.el
+++ config/org-mode.cfg.el
@@ -1,9 +1,12 @@
 ;;======================================================================
 ;; ORG-MODE
 
-(setq auto-mode-alist (append '((".org$" . org-mode)) auto-mode-alist))
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
+;; (add-to-list 'load-path "/usr/share/emacs/25.0.50/lisp/")
+(require 'org-mouse)
 
+;;----------------------------------------------------------------------
+;; Add dot after headline
 ;; https://yoo2080.wordpress.com/2013/08/24/changing-the-number-format-for-section-headings-in-org-mode-html-export/
 (defun my-html-filter-headline-yesdot (text backend info)
   "Ensure dots in headlines."
@@ -21,9 +24,6 @@
   '(progn
      (add-to-list 'org-export-filter-headline-functions
                   'my-html-filter-headline-yesdot)))
-
-;; other hacks
-;; http://kdr2.com/tech/emacs/1405-orgmode-checkbox-unicode.html
 
 ;; (add-to-list 'load-path "~/.emacs.d/el-get/00testing/org-mode/contrib/lisp")
 ;; (load-file "~/.emacs.d/00testing/org-mode/contrib/lisp/org-export.el")
