a9ee7461d74cbc39423070392fc2ebf3f5e80b42
rho <rho.rhoit@gmail.com>
2016-01-17 21:19:51 +0545

    changing init config to org-mode

--- init.el
+++ init.el
@@ -5,190 +5,29 @@
       user-mail-address "rho.rhoit@gmail.com")
 
 ;;======================================================================
-
 ;; debug on C-g; to point broken modules
 ;; (setq debug-on-quit t)
-
-;; UTF-8
-(setq locale-coding-system 'utf-8)
-(set-language-environment 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-;; emacs server client
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
-;; backup configuration
-(setq backup-directory-alist (quote ((".*" . "~/.cache/emacs_backup/"))))
-(setq make-backup-files nil)
-
-;; (setq auto-save-default nil)
-(delete-selection-mode 1)
-
-(setq browse-url-browser-function 'browse-url-firefox)
-(recentf-mode 0) ;; no recent files
-
-;; avoid typing complete 'yes' and 'no
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; highlight entire bracket expression
-(setq show-paren-style 'expression)
-(show-paren-mode 1)
-
-;; mode set
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-
-;; default comment variable
-(setq-default comment-start "# ")
-
-;;----------------------------------------------------------------------
-;; clean up trailing whitespaces
-(defun nuke_traling ()
-  (add-hook 'write-file-hooks 'delete-trailing-whitespace)
-  (add-hook 'before-save-hooks 'whitespace-cleanup))
-
-(add-hook 'prog-mode-hook 'nuke_traling)
-
-;;======================================================================
-;; PROGRAMMING MODES
-
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq-default py-indent-offset 4)
-(add-hook 'prog-mode-hook 'which-function-mode)
-(add-hook 'prog-mode-hook 'toggle-truncate-lines)
-
-;;----------------------------------------------------------------------
-;; CC-mode indentation
-;; http://www.gnu.org/software/emacs/manual/html_mono/ccmode.html
-
-(setq c++-tab-always-indent t)
-(setq c-basic-offset 4)
-(setq c-indent-level 4)
-(c-set-offset 'substatement-open 0)
-(add-hook 'c-mode-common-hook '(lambda () (c-toggle-hungry-state 1)))
-(require 'cc-mode)
-(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
-
-;;======================================================================
-;; PLUGINS
-(add-to-list 'load-path  "~/.emacs.d/plug-ins/")
-
-;;----------------------------------------------------------------------
-;; hideshowvis mode
-;; http://www.emacswiki.org/emacs/download/hideshowvis.el
-(autoload 'hideshowvis-enable "hideshowvis" "Highlight foldable regions")
-(autoload 'hideshowvis-minor-mode
-  "hideshowvis"
-  "Will indicate regions foldable with hideshow in the fringe."
-  'interactive)
-
-(add-hook 'python-mode-hook 'hideshowvis-enable)
-
-
-;;======================================================================
-;; SUB-MODULES PLUGINS
-
-;;----------------------------------------------------------------------
-;; Arch pkgbuild-mode
-(add-to-list 'load-path "~/.emacs.d/repo/pkgbuild-mode")
-(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
-(setq auto-mode-alist
-      (append '(("/PKGBUILD.*" . pkgbuild-mode)) auto-mode-alist))
 
 ;;----------------------------------------------------------------------
 ;; el-get
 ;; https://github.com/dimitri/el-get
-(add-to-list 'load-path "~/.emacs.d/el-get")
-(require 'el-get)
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+;; (add-to-list 'load-path "~/.emacs.d/el-get")
+;; (require 'el-get)
+;; (el-get 'sync)
 
+;; ;; my packages
+;; (setq dim-packages
+;;       (append
+;;        ;; list of packages we use straight from official recipes
+;;        '(org-mode)
 
-;;======================================================================
-;; CONFIGS
+;;        (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources))))
 
-(load "~/.emacs.d/config/el-get.cfg.el")
-;; (load "~/.emacs.d/config/etag.cfg.el")
-(load "~/.emacs.d/config/ido.cfg.el")
-(load "~/.emacs.d/config/ui.cfg.el")
-(load "~/.emacs.d/config/currentline.cfg.el") ;; load after theme
-(load "~/.emacs.d/config/compile.cfg.el")
-(load "~/.emacs.d/config/modeline.cfg.el")
-(load "~/.emacs.d/config/tabbar.cfg.el")
-(load "~/.emacs.d/config/org-mode.cfg.el")
-(load "~/.emacs.d/config/babel.cfg.el")
-(load "~/.emacs.d/config/html.cfg.el")
-(load "~/.emacs.d/config/python.cfg.el")
-
-;;======================================================================
-;; SCRIPTS
-(load "~/.emacs.d/scripts/wordplay.el")
-
-;;======================================================================
-;; TESTING PLUGINS
+;; (el-get 'sync dim-packages)
 
 ;;----------------------------------------------------------------------
-;; window numbering
-(add-to-list 'load-path "~/.emacs.d/00testing/window-numbering/")
-(require 'window-numbering)
-(window-numbering-mode)
-
-;;----------------------------------------------------------------------
-;; indent-hint
-;; (load "~/.emacs.d/plug-ins/indent-hint.el")
-;; (indent-hint t)
-
-;;----------------------------------------------------------------------
-;; highlight indentation
-;; other color: "#aaeeba"
-;; ver1
-(load "~/.emacs.d/00testing/hdent-antonj/highlight-indentation.el")
-(add-hook 'prog-mode-hook 'highlight-indentation-mode)
-;; ver2
-;; (add-to-list 'load-path "~/.emacs.d/00testing/hdent-mgalgs/")
-;; (require 'indent-hints)
-;; (indent-hints-global-mode)
-
-;;----------------------------------------------------------------------
-;; isend-mode
-(add-to-list 'load-path "~/.emacs.d/00testing/isend-mode/")
-(require 'isend)
-
-;;----------------------------------------------------------------------
-;; auto-dim-buffer
-(when window-system
-  (add-to-list 'load-path "~/.emacs.d/00testing/auto-dim-other-buffers.el")
-  (require 'auto-dim-other-buffers)
-  (add-hook 'after-init-hook (lambda ()
-                               (when (fboundp 'auto-dim-other-buffers-mode)
-                                 (auto-dim-other-buffers-mode t)))))
-
-;;----------------------------------------------------------------------
-;; ansi-color sequence for complitaion mode
-(add-to-list 'load-path "~/.emacs.d/00testing/colors")
-(require 'ansi-color)
-(defun colorize-compilation-buffer ()
-  (toggle-read-only)
-  (ansi-color-apply-on-region (point-min) (point-max))
-  (toggle-read-only))
-(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
-
-;;----------------------------------------------------------------------
-;; smart-cursor-color-mode
-;; (add-to-list 'load-path "~/.emacs.d/smart-cursor-color-mode")
-;; (require 'smart-cursor-color-mode)
-;; (setq smart-cursor-color-mode t)
-
-;;----------------------------------------------------------------------
-;; LFG mode
-;; (setq xle-buffer-process-coding-system 'utf-8)
-;; (load-library "/opt/xle/emacs/lfg-mode")
-
+;; load config file
+(org-babel-load-file "~/.emacs.d/config.org")
 
 ;;======================================================================
 ;; EMACS AUTO GEN-STUFFS
@@ -219,7 +58,6 @@
  '(font-lock-string-face ((t (:foreground "gold2" :weight semi-light :family "Source Code Pro"))))
  '(font-lock-variable-name-face ((t (:foreground "sandy brown"))))
  '(font-lock-warning-face ((t (:background "yellow1" :foreground "red1" :weight bold))))
- '(highlight-indentation-face ((t (:background "olive drab"))))
  '(linum ((t (:inherit (shadow default) :height 108))))
  '(markdown-header-delimiter-face ((t (:inherit font-lock-function-name-face :weight bold))) t)
  '(markdown-header-face-1 ((t (:height 1.8))) t)
@@ -229,5 +67,7 @@
  '(markdown-header-face-5 ((t (:height 1.1 :weight bold))) t)
  '(markdown-header-face-6 ((t (:weight bold))) t)
  '(show-paren-match ((t (:inverse-video t))))
+ '(tabbar-default ((t (:background "#c5c5c5"))))
+ '(tabbar-button ((t (:background "#c5c5c5"))))
  '(which-func ((t (:background "gray40")))))
 (put 'narrow-to-region 'disabled nil)
