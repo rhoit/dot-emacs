9369680c5aaebd79d06e54ed4696a49d5f822b40
rho <rho.rhoit@gmail.com>
2016-01-23 17:24:18 +0545

    separating modules
    - clean up

--- config/ui.cfg.el
+++ config/ui.cfg.el
@@ -7,7 +7,7 @@
 (add-to-list 'default-frame-alist '(width . 104))
 
 ;;----------------------------------------------------------------------
-;;; Ui entity
+;;; UI entity
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
@@ -72,7 +72,7 @@
 							   (text-scale-mode -1)))
 
 ;;----------------------------------------------------------------------
-;;; undo action for gui
+;;; undo action for GUI
 
 (when window-system
   (require 'undo-tree)
@@ -106,3 +106,30 @@
                         (toggle-truncate-lines)
                         (fci-mode)))
 (global-set-key [f9] 'speedbar)
+
+;;======================================================================
+;;; currentline
+;; http://stackoverflow.com/questions/10239037/emacs-hl-line-change-color-locally
+;; only load after theme is done
+(defun shade-color (intensity)
+  "print the #rgb color of the background, dimmed according to intensity"
+  (interactive "nIntensity of the shade : ")
+  (apply 'format "#%02x%02x%02x"
+         (mapcar (lambda (x)
+                   (if (> (lsh x -8) intensity)
+                       (- (lsh x -8) intensity)
+                     0))
+                 (color-values (cdr (assoc 'background-color (frame-parameters)))))))
+
+;;----------------------------------------------------------------------
+;; Default hl
+;; (global-hl-line-mode t)
+
+;;----------------------------------------------------------------------
+;; highline mode
+(require 'highline)
+
+;; (set-face-background 'highline-face "#111")
+(set-face-background 'highline-face (shade-color 09))
+(add-hook 'prog-mode-hook 'highline-mode-on)
+
