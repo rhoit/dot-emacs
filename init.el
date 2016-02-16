766e8d67f07450926dae697d1958145a803dd034
rho <rho.rhoit@gmail.com>
2016-02-16 15:47:54 +0545

    config file as README now

--- init.el
+++ init.el
@@ -2,12 +2,15 @@
 ;;; emacs config file for 25.0
 ;;======================================================================
 
+(setq user-full-name "rho"
+      user-mail-address "rho.rhoit@gmail.com")
+
 ;; debug on C-g; to point broken modules
 ;; (setq debug-on-quit t)
 
 ;;----------------------------------------------------------------------
 ;;; load main config via org
-(org-babel-load-file "~/.emacs.d/config.org")
+(org-babel-load-file "~/.emacs.d/README.org")
 
 ;;======================================================================
 ;;; EMACS AUTO GEN-STUFFS
