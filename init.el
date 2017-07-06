58e60f2f44984f1d49793ef36b0193cb61f655cd
rho <rho.rhoit@gmail.com>
2017-07-06 14:49:52 +0545

    core - package load method

--- init.el
+++ init.el
@@ -5,8 +5,18 @@
 ;; (setq debug-on-quit t)
 
 ;;----------------------------------------------------------------------
+;;; package.el
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/elpa/")
+
+;;----------------------------------------------------------------------
 ;;; load main config via org
-(org-babel-load-file "~/.emacs.d/README.org")
+(add-hook 'after-init-hook (lambda () (org-babel-load-file "~/.emacs.d/README.org")))
+
 
 ;;======================================================================
 ;;; EMACS AUTO GEN-STUFFS
