c6c3485a919efa9025b48fa5ea7e00913bc86287
rho <rho.rhoit@gmail.com>
2017-07-09 17:31:51 +0545

    fix web-mode

--- config/html.cfg.el
+++ config/html.cfg.el
@@ -2,52 +2,6 @@
 ;; HTML stuffs
 
 ;;----------------------------------------------------------------------
-;; web-mode configs
-
-;; NOTE: if you are antonj's highlight-indentation-mode
-;;       please use the *localreadhead* fork
-;; https://github.com/capitaomorte/yasnippet/issues/396
-
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml?\\'" . web-mode))
-
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-style-padding 2)
-(setq web-mode-script-padding 2)
-(setq web-mode-attr-indent-offset 2)
-(setq web-mode-enable-block-face t)
-(setq web-mode-enable-current-column-highlight t)
-
-;; ya-snippet completion for web-mode
-(add-hook 'web-mode-hook #'(lambda () (yas-activate-extra-mode 'html-mode)))
-
-;;----------------------------------------------------------------------
-;; mozrepl
-;; (autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
-;; (add-hook 'javascript-mode-hook 'javascript-custom-setup)
-;; (defun javascript-custom-setup ()
-;;   (moz-minor-mode 1))
-
-;; http://www.emacswiki.org/emacs/MozRepl
-(defun auto-reload-firefox-on-after-save-hook ()
-  (add-hook 'after-save-hook
-            '(lambda ()
-               (interactive)
-               (when (not (bound-and-true-p moz-minor-mode))
-                 (return))
-               (comint-send-string (inferior-moz-process)
-                                   "setTimeout(BrowserReload(), \"1000\");"))
-            'append 'local)) ; buffer-local
-
-(add-hook 'html-mode-hook 'auto-reload-firefox-on-after-save-hook)
-(add-hook 'css-mode-hook 'auto-reload-firefox-on-after-save-hook)
-(add-hook 'org-mode-hook 'auto-reload-firefox-on-after-save-hook)
-(add-hook 'web-mode-hook 'auto-reload-firefox-on-after-save-hook)
-(add-hook 'programming-mode-hook 'auto-reload-firefox-on-after-save-hook)
-
-;;----------------------------------------------------------------------
 ;; html-mode hex value show color
 (defvar hexcolour-keywords
   '(("#[abcdef[:digit:]]\\{3,6\\}"
@@ -66,7 +20,7 @@
 										 )))))
 		append))))
 
-(defun hexcolour-add-to-font-lock ()
-  (interactive)
-  (font-lock-add-keywords nil hexcolour-keywords))
-(put 'scroll-left 'disabled nil)
+
+;;(defun hexcolour-add-to-font-lock ()
+;;  (interactive)
+;;  (font-lock-add-keywords nil hexcolour-keywords))
