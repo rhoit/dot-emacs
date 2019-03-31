bcfc53c03fdadfd66f2e8d0c881dee4196837592
rho <rho.rhoit@gmail.com>
2019-03-31 21:20:04 +0545

    core - elpa conf

--- init.el
+++ init.el
@@ -7,6 +7,15 @@
 
 
 ;;----------------------------------------------------------------------
+;;; ELPA
+;; Added by Package.el. This must come before configurations of
+;; installed packages. Don't delete this line. If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
+
+;;----------------------------------------------------------------------
 ;;; load main config via org
 ;; (load-file "~/.emacs.d/README.el")
 (add-hook 'after-init-hook (lambda () (org-babel-load-file "~/.emacs.d/README.org")))
