a713ac6e903d00353f32e52bb7be9f3fbfa33e28
rho <rho.rhoit@gmail.com>
2020-08-27 14:52:20 +0545

    early-init

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;;----------------------------------------------------------------------
+;;; window
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+
+;;----------------------------------------------------------------------
+;;; themes
+
+(load-theme 'wombat t)
