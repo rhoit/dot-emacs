cebc357bdd4340d384d8f25860ef97839816e2d7
rho <rho.rhoit@gmail.com>
2021-06-25 10:59:37 +0545

    core - gc hack

--- early-init.el
+++ early-init.el
@@ -1,4 +1,11 @@
 ;;----------------------------------------------------------------------
+;;; garbage collector
+;; suppress gc temporarily stolen from doom-emacs
+
+(setq gc-cons-threshold most-positive-fixnum) ; 2^61 bytes
+(setq gc-cons-percentage 0.6)
+
+;;----------------------------------------------------------------------
 ;;; window
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
