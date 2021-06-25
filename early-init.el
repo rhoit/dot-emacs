7cc3a496472a6e59b65f16fe2b3a2f333a45ba6d
rho <rho.rhoit@gmail.com>
2021-06-25 10:59:37 +0545

    core - gc hack

--- early-init.el
+++ early-init.el
@@ -2,6 +2,10 @@
 ;;; early-init file of emacs 27.1
 ;;======================================================================
 
+;;----------------------------------------------------------------------
+;; suppress gc temporarily; stolen from doom-emacs
+(setq gc-cons-threshold most-positive-fixnum) ; 2^61 bytes
+(setq gc-cons-percentage 0.6)
 
 ;;----------------------------------------------------------------------
 ;;; Window
