9f94027cba197598b6339459e14dc411525c5e2f
rho <rho.rhoit@gmail.com>
2022-01-12 07:03:43 +0545

    early-init.el

---
+++ early-init.el
@@ -0,0 +1,23 @@
+;;----------------------------------------------------------------------
+;;; native complie since v28
+
+(setq package-native-compile t)
+
+;;----------------------------------------------------------------------
+;;; garbage collector
+;; suppress gc temporarily stolen from doom-emacs
+
+(setq gc-cons-threshold most-positive-fixnum) ; 2^61 bytes
+(setq gc-cons-percentage 0.6)
+
+;;----------------------------------------------------------------------
+;;; window
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;;----------------------------------------------------------------------
+;;; themes
+
+(load-theme 'wombat t)
