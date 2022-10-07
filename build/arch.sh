ebabf12ce49c187931c4e336b7a03e75878d0b42
rho <rho.rhoit@gmail.com>
2022-10-07 11:46:36 +0545

    build - change source to github

--- build/arch.sh
+++ build/arch.sh
@@ -7,8 +7,9 @@ if test -e repo; then
     (cd repo; git fetch --deepen 1; git reset --hard origin/master)
 else
     # `--no-single-branch` to get all branches
-    git clone --single-branch --depth=1 "git://git.savannah.gnu.org/emacs.git" repo
+    # git clone --single-branch --depth=1 "git://git.savannah.gnu.org/emacs.git" repo
+    git clone --single-branch --depth=1 "https://github.com/emacs-mirror/emacs.git" repo
 fi
 
-
-makepkg --force --install
+# pkg-config --list-all  # run if dependencies installed but not found
+makepkg --syncdeps --force --install
